53a6e9728baec4261818014f3d34c2131874f32c
gcmartin <gscalica@gmail.com>
2020-07-16 16:37:17 -0700

    Create assets, components, and containers folders and Ball component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Ball from './components/Ball';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Ball />
     </div>
   );
 }
---
+++ src/components/Ball.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Ball() {
+  return(
+    <h1>This is the ball component</h1>
+  );
+}
+
+export default Ball;
