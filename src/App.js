571a2c17549a6fca4cc95728256657ea8e6e4fd2
gcmartin <gscalica@gmail.com>
2020-07-17 14:58:40 -0700

    Pass 'title' prop to Lottery component in App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Lottery numBalls={6} maxNum={40} />
-      <Lottery numBalls={4} maxNum={10} />
+      <Lottery numBalls={4} maxNum={10} title="Mini-Daily" />
     </div>
   );
 }
