5e95e59bacd6d87ec873aa061648598dca8625ee
gcmartin <gscalica@gmail.com>
2020-07-16 17:15:39 -0700

    Add defaultProps and change title to props

--- src/containers/Lottery.js
+++ src/containers/Lottery.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 import Ball from './../components/Ball';
 
 class Lottery extends Component {
+  static defaultProps = {
+    title: "Lottery"
+  }
+
   render() {
     return (
       <div>
-        <h1>Lottery</h1>
+        <h1>{this.props.title}</h1>
         <Ball />
         <Ball />
         <Ball />
